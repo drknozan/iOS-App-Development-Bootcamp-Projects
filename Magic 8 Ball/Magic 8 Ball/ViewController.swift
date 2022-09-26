a8924505963c434b90f1f32bd369c23513625762
drknozan <mozandrkn@gmail.com>
2022-09-26 19:50:50 +0300

    first commit

---
+++ Magic 8 Ball/Magic 8 Ball/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Magic 8 Ball
+//
+//  Created by Angela Yu on 14/06/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    let ballArray = [UIImage(imageLiteralResourceName: "ball1"), UIImage(imageLiteralResourceName: "ball2"), UIImage(imageLiteralResourceName: "ball3"), UIImage(imageLiteralResourceName: "ball4"), UIImage(imageLiteralResourceName: "ball5")]
+
+    @IBOutlet weak var imageView: UIImageView!
+
+    @IBAction func askButtonPressed(_ sender: UIButton) {
+        imageView.image = ballArray[Int.random(in: 0...4)]
+    }
+}
+
---
+++ README.md
@@ -0,0 +1,9 @@
+# iOS App Development Bootcamp Projects
+
+This repo contains the apps and challenges I completed while taking "iOS & Swift - The Complete iOS App Development Bootcamp" course by Dr. Angela Yu on Udemy.
+
+## 8 Magic Ball
+
+> ![](./img/8magicball.gif)
+
+> When the user clicks the ask button, app selects random answer from an array of UIImages and displays it on the ImageView.
